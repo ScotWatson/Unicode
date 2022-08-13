dcbb812903b35d8c57f8a3e88ab9ca535a8f071e
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-08-13 08:04:40 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1325,7 +1325,7 @@ function save(arrStr) {
     + "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
     + "*/\n"
     + "\n";
-  const blob = new Blob( [ header, ...arrStr ] );
+  const blob = new Blob( [ header, ...arrStr ], {encoding:"UTF-8", type: "text/plain;charset=UTF-8"} );
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
@@ -1342,7 +1342,7 @@ function createSaveLink(arrStr, name) {
     + "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
     + "*/\n"
     + "\n";
-  const blob = new Blob( [ header, ...arrStr ] );
+  const blob = new Blob( [ header, ...arrStr ], {encoding:"UTF-8", type: "text/plain;charset=UTF-8"} );
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
