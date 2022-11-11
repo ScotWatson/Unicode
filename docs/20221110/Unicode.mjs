494f690e770e77768d43888efe01c2229dea92d6
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-11-10 23:21:13 -0500

    Update Unicode.mjs

--- docs/20221110/Unicode.mjs
+++ docs/20221110/Unicode.mjs
@@ -3,8 +3,9 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-import * as ErrorHandling from "https://scotwatson.github.io/ErrorHandling/ErrorHandling.mjs";
-import * as Category_1_1_5 from "https://scotwatson.github.io/Unicode/category-1.1.5.mjs";
+import * as Types from "https://scotwatson.github.io/Debug/20221107/Types.mjs";
+import * as ErrorHandling from "https://scotwatson.github.io/Debug/20221107/ErrorHandling.mjs";
+import * as Category_1_1_5 from "https://scotwatson.github.io/Unicode/20221110/category-1.1.5.mjs";
 
 // Code points are assigned/designated to (abstract) characters, or given a normative function.
 // Normalization maintains the sequence of abstract characters, but may change the sequence of code points.
@@ -13,59 +14,94 @@ import * as Category_1_1_5 from "https://scotwatson.github.io/Unicode/category-1
 export class CodePoint {
   #str;
   constructor(args) {
-    let value;
-    if (typeof args === "number") {
-      value = args;
-    } else if (ErrorHandling.isBareObject(args)) {
-      if (!(args.hasOwnProperty("value"))) {
-        throw new Error("Invalid Arguments");
+    try {
+      let value;
+      if (Types.isInteger(args)) {
+        value = args;
+      } else if (Types.isSimpleObject(args)) {
+        if (!(Object.hasOwn(args, "value"))) {
+          throw "Invalid Arguments";
+        }
+        if (Types.isInteger(args.value)) {
+          throw "Invalid Arguments: value must be a number";
+        }
+        value = args.value;
+      } else {
+        throw "Invalid Arguments";
       }
-      if (typeof args.value !== "number") {
-        throw new Error("Invalid Arguments: value must be a number");
+      if (!(Types.isInteger(value))) {
+        throw "Invalid Arguments: value must be an integer";
       }
-      value = args.value;
-    } else {
-      throw new Error("Invalid Arguments");
+      if (value < 0 || value > 0x10FFFF) {
+        throw "Invalid Arguments: value must be between 0 and 0x10FFFF, inclusive";
+      }
+      this.#str = String.fromCodePoint(value);
+    } catch (e) {
+      ErrorLog.rethrow({
+        functionName: "CodePoint constructor",
+        error: e,
+      });
     }
-    if (!(Number.isInteger(value))) {
-      throw new Error("Invalid Arguments: value must be an integer");
-    }
-    if (value < 0 || value > 0x10FFFF) {
-      throw new Error("Invalid Arguments: value must be between 0 and 0x10FFFF, inclusive");
-    }
-    this.#str = String.fromCodePoint(value);
   }
   valueOf() {
-    this.#str.codePointAt(0);
+    try {
+      this.#str.codePointAt(0);
+    } catch (e) {
+      ErrorLog.rethrow({
+        functionName: "CodePoint.valueOf",
+        error: e,
+      });
+    }
   }
   toString() {
-    return this.#str;
+    try {
+      return this.#str;
+    } catch (e) {
+      ErrorLog.rethrow({
+        functionName: "CodePoint.toString",
+        error: e,
+      });
+    }
   }
   get category() {
-    return Category_1_1_5.generalCategory_1_1_5[this.#str.codePointAt(0)];
+    try {
+      return Category_1_1_5.generalCategory_1_1_5[this.#str.codePointAt(0)];
+    } catch (e) {
+      ErrorLog.rethrow({
+        functionName: "get CodePoint.category",
+        error: e,
+      });
+    }
   }
   static fromSurrogatePair(args) {
-    if (!ErrorHandling.isBareObject(args)) {
-      throw new Error("Invalid Arguments");
+    try {
+      if (!(Types.isSimpleObject(args))) {
+        throw "Invalid Arguments";
+      }
+      if (!(Object.hasOwn(args, "lead"))) {
+        throw "Argument \"lead\" must be provided.";
+      }
+      if (!(Types.isInteger(args.lead))) {
+        throw "Argument \"lead\" must be an integer.";
+      }
+      if (args.lead >= 0xD800 && args.lead < 0xDC00) {
+        throw "Argument \"lead\" must be between 0xD800 and 0xDC00.";
+      }
+      if (!(Object.hasOwn(args, "trail"))) {
+        throw "Argument \"trail\" must be provided.";
+      }
+      if (!(Types.isInteger(args.trail))) {
+        throw "Argument \"trail\" must be an integer.";
+      }
+      if (args.trail >= 0xDC00 && args.trail < 0xE000) {
+        throw "Argument \"lead\" must be between 0xDC00 and 0xE000.";
+      }
+      return new CodePoint((args.lead - 0xD800) * 0x400 + (args.trail - 0xDC00) + 0x10000);
+    } catch (e) {
+      ErrorLog.rethrow({
+        functionName: "static CodePoint.fromSurrogatePair",
+        error: e,
+      });
     }
-    if (!(args.hasOwnProperty("lead"))) {
-      throw new Error("Invalid Arguments");
-    }
-    if (typeof args.lead !== "number") {
-      throw new Error("Invalid Arguments: lead must be a number");
-    }
-    if (args.lead >= 0xD800 && args.lead < 0xDC00) {
-      throw new Error("Invalid Arguments: lead must be between 0xD800 and 0xDC00");
-    }
-    if (!(args.hasOwnProperty("trail"))) {
-      throw new Error("Invalid Arguments");
-    }
-    if (typeof args.trail !== "number") {
-      throw new Error("Invalid Arguments: trail must be a number");
-    }
-    if (args.trail >= 0xDC00 && args.trail < 0xE000) {
-      throw new Error("Invalid Arguments: trail must be between 0xDC00 and 0xE000");
-    }
-    return new CodePoint((lead - 0xD800) * 0x400 + (trail - 0xDC00) + 0x10000);
   }
 }
