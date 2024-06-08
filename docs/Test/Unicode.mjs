537c1e482a6cb9ea83854cc77d7a03f7b8508a24
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-07 21:38:13 -0400

    Update Unicode.mjs

--- docs/Test/Unicode.mjs
+++ docs/Test/Unicode.mjs
@@ -1,5 +1,5 @@
 /*
-(c) 2023 Scot Watson  All Rights Reserved
+(c) 2024 Scot Watson  All Rights Reserved
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -10,6 +10,120 @@ import * as Category_1_1_5 from "https://scotwatson.github.io/Unicode/Test/categ
 // A Unicode code point is a value in the Unicode codespace (integers from 0 to 0x10FFFF)
 // Not all code points are assigned to encoded characters.
 // Code Point Types: Graphic, Format, Control, Private-Use, Surrogate, Noncharacter, Reserved
+
+export class USVString {
+  #value;
+  #length;
+  constructor(args) {
+    this.#value = "":
+    this.length = 0;
+    append(args);
+  }
+  append(args) {
+    const { value, length } = getString(args);
+    this.#value += value;
+    this.#length += length;
+    function getString(args) {
+      switch (typeof args) {
+        case "number": {
+          const codePoint = args;
+          if (value < 0 || value > 0x10FFFF) {
+            throw "Invalid Arguments: value must be between 0 and 0x10FFFF, inclusive";
+          }
+          if (CodePoint.isSurrogate(codePoint)) {
+            throw "Surrogate code point is not allowed in Unicode Scalar Value string: " + codePoint;
+          }
+          return {
+            value: String.fromCodePoint(codePoint),
+            length: 1,
+          };
+        }
+          break;
+        case "string": {
+          let pair = false;
+          let length = 0;
+          // In Javascript string type, code unit is UTF-16
+          for (const codeUnit of str) {
+            if (pair) {
+              if (codeUnit >= 0xD800 && codeUnit < 0xDC00) {  // is lead surrogate
+                throw "two lead surrogates";
+              } else if (codeUnit >= 0xDC00 && codeUnit < 0xE000) {  // is trail surrogate
+                // valid surrogate pair
+                length += 1;
+              } else {
+                throw "lead surrogate without trail surrogate";
+              }
+            } else {
+              if (codeUnit >= 0xD800 && codeUnit < 0xDC00) {  // is lead surrogate
+                // start of surrogate pair
+                pair = true;
+              } else if (codeUnit >= 0xDC00 && codeUnit < 0xE000) {  // is trail surrogate
+                return "trail surrogate without lead surrogate";
+              } else {
+                length += 1;
+              }
+            }
+          }
+          if (pair) {
+            throw "lead surrogate without trail surrogate";
+          }
+          return {
+            value: args,
+            length,
+          };
+        }
+          break;
+        case "object": {
+          let totalValue = "";
+          let totalLength = 0;
+          if (!args[Symbol.iterator]) {
+            throw "Object must be iterable";
+          }
+          for (const obj of args) {
+            ({ value, length } = getString(obj));
+          }
+          return {
+            value: totalValue,
+            length: totalLength,
+          };
+        }
+          break;
+        default: {
+          throw "Connot construct string from this type.";
+        }
+      }
+    }
+  }
+  concat(other) {
+    this.#value += other.toString();
+  }
+  get length() {
+    return length;
+  }
+  toString() {
+    return this.#value;
+  }
+  *[Symbol.iterator]() {
+    let pair;
+    // In Javascript string type, code unit is UTF-16
+    for (const codeUnit of this.#value) {
+      if (pair) {
+        // Due to internal validation, this can be assumed to be a trail surrogate
+        pair.trail = codeUnit;
+        yield CodePoint.fromSurrogatePair(pair);
+        pair = undefined;
+      } else {
+        if (CodePoint.isSurrogate(codeUnit)) {
+          // Due to internal validation, this can be assumed to be a lead surrogate
+          pair = { lead: codeUnit };
+        } else {
+          yield codeUnit;
+        }
+      }
+    }
+    return;
+  }
+}
 
 export class CodePoint {
   #value;
@@ -63,9 +177,9 @@ export class CodePoint {
       });
     }
   }
-  get category() {
+  static getCategory(codePoint) {
     try {
-      return Category_1_1_5.generalCategory_1_1_5[this.#value];
+      return Category_1_1_5.generalCategory_1_1_5[codePoint];
     } catch (e) {
       ErrorLog.rethrow({
         functionName: "get CodePoint.category",
@@ -73,11 +187,11 @@ export class CodePoint {
       });
     }
   }
-  isSurrogate() {
-    return ((this.#value > 0xD7FF) && (this.#value < 0xE000));
+  static isSurrogate(codePoint) {
+    return ((codePoint > 0xD7FF) && (codePoint < 0xE000));
   }
-  isScalar() {
-    return !(this.isSurrogate());
+  static isScalar() {
+    return !(isSurrogate(codePoint));
   }
   static fromSurrogatePair(args) {
     try {
@@ -100,9 +214,9 @@ export class CodePoint {
         throw "Argument \"trail\" must be an integer.";
       }
       if (args.trail >= 0xDC00 && args.trail < 0xE000) {
-        throw "Argument \"lead\" must be between 0xDC00 and 0xE000.";
+        throw "Argument \"trail\" must be between 0xDC00 and 0xE000.";
       }
-      return new CodePoint((args.lead - 0xD800) * 0x400 + (args.trail - 0xDC00) + 0x10000);
+      return (args.lead - 0xD800) * 0x400 + (args.trail - 0xDC00) + 0x10000;
     } catch (e) {
       ErrorLog.rethrow({
         functionName: "static CodePoint.fromSurrogatePair",
